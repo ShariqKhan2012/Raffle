b93947c52d9393ea57b1736dd188a0e6d4300aee
Shariq Hasan Khan <shariqkhanfreelancer@gmail.com>
2026-08-30 16:33:40 +0530

    Production build shound not try to connect to Anvil

--- ui/src/lib/wagmi.ts
+++ ui/src/lib/wagmi.ts
@@ -1,11 +1,11 @@
 import { connectorsForWallets } from "@rainbow-me/rainbowkit";
 import {
-  injectedWallet,
   coinbaseWallet,
+  injectedWallet,
   walletConnectWallet,
 } from "@rainbow-me/rainbowkit/wallets";
 import { createConfig, http } from "wagmi";
-import { anvil, mainnet, sepolia } from "wagmi/chains";
+import { anvil, sepolia } from "wagmi/chains";
 
 const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!;
 
@@ -19,13 +19,13 @@ const connectors = connectorsForWallets(
   { appName: "VRF Raffle", projectId }
 );
 
+const chains = process.env.NODE_ENV === "production" ? ([sepolia] as const) : ([anvil, sepolia] as const);
 export const config = createConfig({
-  chains: [anvil, sepolia, mainnet],
+  chains,
   connectors,
   transports: {
     [anvil.id]: http("http://127.0.0.1:8545"),
     [sepolia.id]: http(),
-    [mainnet.id]: http("https://cloudflare-eth.com"),
   },
   ssr: true,
 });
